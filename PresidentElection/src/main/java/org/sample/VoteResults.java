e1d3c21c1e5fbd0c40f6847bdcb2f6ddca5bb4d2
Samotessov <vsamotessov@invidi.com>
2022-02-20 12:06:36 -0500

    Update signature1

--- PresidentElection/src/main/java/org/sample/VoteResults.java
+++ PresidentElection/src/main/java/org/sample/VoteResults.java
@@ -1,11 +1,8 @@
 package org.sample;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.FileReader;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -15,8 +12,7 @@ import java.util.stream.Collectors;
 public class VoteResults {
 
     private Map<String, Integer> winners(String fullFileName) throws FileNotFoundException {
-        InputStream is = new FileInputStream(new File(fullFileName));
-        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        BufferedReader br = new BufferedReader(new FileReader(fullFileName));
         return br.lines().
                 map(mapToItem).
                 filter(x -> x.getOffice().contains("PRESIDENT AND VICE PRESIDENT OF THE UNITED STATES")).
