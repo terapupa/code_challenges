62fd2163d6fd30b9bf6d5026364d0e4d83ad9d4b
Samotessov <vsamotessov@invidi.com>
2022-02-20 11:58:05 -0500

    Update signature

--- SimpleParserSpring/src/main/java/com/ef/domain/RequestRepository.java
+++ SimpleParserSpring/src/main/java/com/ef/domain/RequestRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 public interface RequestRepository extends JpaRepository<Request, Long> {
 
     @Query(value = "SELECT ip, count(ip) as number FROM request WHERE (time_stamp>?1 AND time_stamp<?2) GROUP BY ip HAVING number>?3", nativeQuery = true)
-    public List<?> findExceededIps(String startTimeStamp, String endTimeStamp, int threshold);
+    List<?> findExceededIps(String startTimeStamp, String endTimeStamp, int threshold);
 }
